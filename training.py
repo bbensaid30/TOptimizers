5f6058d98ed608dceec5b89a158bb7799aebfebc
Bensaid <bilel.bensaid30@gmail.com>
2026-03-17 16:27:25 +0100

    change names algo

--- training.py
+++ training.py
@@ -8,16 +8,20 @@ import variance_reduction
 def train(algo,model,loss_fn,
 x,y, eps, max_epochs, lr=0.1, seuil=0.01, f1=2, f2=10000, rho=0.9, eps_egd=0.01, lambd=0.5, 
 beta_1 = 0.9, beta_2=0.999, epsilon_a=1e-07, amsgrad=False, typef="float32", sample_weight=1):
-    if(algo=="LC_EGD"):
-        return perso.LC_EGD(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,typef,sample_weight)
+    if(algo=="LC_GD"):
+        return perso.LC_GD(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,typef,sample_weight)
     elif(algo=="LC_NGD"):
         return perso.LC_NGD(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,typef,sample_weight)
-    elif(algo=="LC_EGD2"):
-        return perso.LC_EGD2(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,typef,sample_weight)
-    elif(algo=="LC_Momentum"):
-        return perso.LC_Momentum(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,beta_1,typef,sample_weight)
+    elif(algo=="LCD_GD"):
+        return perso.LCD_GD(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,typef,sample_weight)
+    elif(algo=="LC_Mom"):
+        return perso.LC_Mom(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,beta_1,typef,sample_weight)
+    elif(algo=="LCD_Mom"):
+        return perso.LCD_Mom(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,beta_1,typef,sample_weight)
     elif(algo=="LC_RMS"):
         return perso.LC_RMS(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,beta_2,epsilon_a,typef,sample_weight)
+    elif(algo=="LCD_RMS"):
+        return perso.LCD_RMS(model,loss_fn,x,y,eps,max_epochs,lr,f1,f2,lambd,beta_2,epsilon_a,typef,sample_weight)
     elif(algo=="Momentum"):
         return classic.Momentum(model,loss_fn,x,y,eps,max_epochs,lr,beta_1,sample_weight)
     elif(algo=="Adam"):
