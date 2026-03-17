850d977e6d8483c211064fabff32fc9d04ddbd3d
Bensaid <bilel.bensaid30@gmail.com>
2026-03-17 18:54:06 +0100

    scripts for MNIST and FMNIST with the tirages json updates

--- main_FashionMnist.py
+++ main_FashionMnist.py
@@ -1,7 +1,7 @@
 from keras import losses, metrics
 
 from data import FASHION_MNIST
-import tirages
+from tirages_json import tirages_json
 
 sample_weight=1
 
@@ -19,26 +19,22 @@ name_init="Bengio"
 params_init=[-1,1]
 
 #paramètres d'arrêt
-eps=10**(-4); max_epochs=3000
+eps=10**(-4); max_epochs=10000
 #paramètres d'entrainement 
-lr=0.001
+lr=0.1
 seuil=0.01
 f1=30; f2=10000; lambd=0.5; rho=0.9; eps_egd=0.01
-beta_1=0.9; beta_2=0.999; epsilon=1e-07
+beta_1=0.9; beta_2=0.999; epsilon=1e-10
 amsgrad=False
 
-tirageMin=0; nbTirages=1
-algo="LC_EGD2"
-studies = tirages.tirages(tirageMin,nbTirages,name_model,nbNeurons,activations,loss,name_init,params_init,metrics,
+tirageMin=0; nbTirages=50
+algo="LCD_RMS"
+
+folder="FASHION_MNIST/"
+filename=folder+algo+".jsonl"
+
+studies = tirages_json(filename, tirageMin,nbTirages,name_model,nbNeurons,activations,loss,name_init,params_init,metrics,
 x_train,y_train,algo,eps,max_epochs,lr,seuil,f1,f2,rho,eps_egd,lambd,beta_1,beta_2,epsilon,amsgrad,"float32",sample_weight,
 "simple",x_test,y_test)
-print(studies)
-
-""" fileEnd = tirages.informationFile(tirageMin,nbTirages,name_model, nbNeurons, activations, name_init, params_init,
-60000, 10000,
-algo, eps, max_epochs, lr, seuil, f1, f2, rho, eps_egd,lambd, beta_1, beta_2, epsilon)
-
-folder="FASHION_MNIST"
-tirages.minsRecordRegression(studies,folder,fileEnd,eps) """
 
 
--- main_MNIST.py
+++ main_MNIST.py
@@ -1,17 +1,17 @@
 from keras import losses, metrics
 
-from data import MNIST_flatten
-import tirages
+from data import MNIST
+from tirages_json import tirages_json
 
 sample_weight=1
 
 # Prepare the training dataset.
-x_train, y_train, x_test, y_test = MNIST_flatten(typef)
+x_train, y_train, x_test, y_test = MNIST()
 
 # architecture.
-name_model="FC"
-nbNeurons=[784,24,10]
-activations=['tanh','softmax']
+name_model="lenet1_mnist"
+nbNeurons=[]
+activations=[]
 loss = losses.MeanSquaredError()
 #loss = losses.CategoricalCrossentropy()
 metrics = ["categorical_accuracy"]
@@ -19,26 +19,23 @@ name_init="Bengio"
 params_init=[-1,1]
 
 #paramètres d'arrêt
-eps=10**(-4); max_epochs=100
+eps=10**(-4); max_epochs=20000
 #paramètres d'entrainement 
 lr=0.1
 seuil=0.01
 f1=30; f2=10000; lambd=0.5; rho=0.9; eps_egd=0.01
-beta_1=0.9; beta_2=0.999; epsilon=1e-07
+beta_1=0.9; beta_2=0.999; epsilon=1e-10
 amsgrad=False
 
-tirageMin=0; nbTirages=1
-algo="LC_EGD2"
-studies = tirages.tirages(tirageMin,nbTirages,name_model,nbNeurons,activations,loss,name_init,params_init,metrics,
+tirageMin=0; nbTirages=50
+algo="LCD_RMS"
+
+folder="MNIST/"
+filename=folder+algo+".jsonl"
+
+studies = tirages_json(filename, tirageMin,nbTirages,name_model,nbNeurons,activations,loss,name_init,params_init,metrics,
 x_train,y_train,algo,eps,max_epochs,lr,seuil,f1,f2,rho,eps_egd,lambd,beta_1,beta_2,epsilon,amsgrad,"float32",sample_weight,
 "simple",x_test,y_test)
-print(studies)
-
-fileEnd = tirages.informationFile(tirageMin,nbTirages,name_model, nbNeurons, activations, name_init, params_init,
-60000, 10000, algo, eps, max_epochs, lr, seuil, f1, f2, rho, eps_egd,lambd, beta_1, beta_2, epsilon)
-
-#folder="MNIST"
-#tirages.minsRecordRegression(studies,folder,fileEnd,eps)
 
 
 
